90aa1fed3503318c0e517dba8fdca3d0a57265de
Ken Wakita <wakita@is.titech.ac.jp>
2011-06-17 18:24:23 +0900

    manifest.json を拡張機能を実装する JS から生成することとした．

--- chrome.x/vpx.js
+++ chrome.x/vpx.js
@@ -1,1 +1,28 @@
-alert('VP/X activated!');
+(function (args) {
+    var manifest = {
+      name: 'VP/X extention',
+      version: '1.0',
+      description: 'An extention for VP/X project',
+      // default_locale: 'en',
+
+      permissions: [ 'experimental', 'tabs' ],
+
+      content_scripts: [
+      {
+        matches: [ 'http://localhost/~wakita/projects/vpx/' + '*' ],
+        js: [ 'vpx.js' ]
+      } ],
+    };
+
+    if (typeof(alert) !== 'undefined')
+      alert('VP/X activated!');
+    else {
+      for (i in args) {
+        switch (args[i]) {
+        case '--manifest':
+          print(JSON.stringify(manifest, null, 2));
+          break;
+        }
+      }
+    }
+  })(typeof(arguments) !== 'undefined' ? arguments : []);
