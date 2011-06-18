051f19a2cc42042d387bee5ac881951f2c9e777e
Ken Wakita <wakita@is.titech.ac.jp>
2011-06-18 10:56:05 +0900

    global script を追加．
    global script を含む background_page と，ページに埋め込まれる injected script の別を理解した．

---
+++ chrome.x/inject.js
@@ -0,0 +1,3 @@
+(function () {
+    console.log('Inject script launched.');
+  })();
--- chrome.x/vpx.js
+++ chrome.x/vpx.js
@@ -1,3 +1,5 @@
+// Manifest definition
+
 (function (args) {
     var manifest = {
       name: 'VP/X extention',
@@ -5,18 +7,18 @@
       description: 'An extention for VP/X project',
       // default_locale: 'en',
 
-      permissions: [ 'experimental', 'tabs' ],
+      permissions: [ 'contextMenus', 'experimental', 'tabs' ],
+
+      background_page: 'vpx.html',
 
       content_scripts: [
       {
         matches: [ 'http://localhost/~wakita/projects/vpx/' + '*' ],
-        js: [ 'vpx.js' ]
+        js: [ 'inject.js' ]
       } ],
     };
 
-    if (typeof(alert) !== 'undefined')
-      alert('VP/X activated!');
-    else {
+    if (typeof(alert) === 'undefined') {
       for (i in args) {
         switch (args[i]) {
         case '--manifest':
@@ -26,3 +28,10 @@
       }
     }
   })(typeof(arguments) !== 'undefined' ? arguments : []);
+
+// Global script
+
+(function () {
+    if (typeof(window) === 'undefined') return;
+    console.log('VP/X launched');
+  })();
