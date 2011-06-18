60738270c895676475d53424842cb8601e9ad30f
Ken Wakita <wakita@is.titech.ac.jp>
2011-06-18 19:03:28 +0900

    Runtime.evaluate に成功！

--- chrome.x/vpx.js
+++ chrome.x/vpx.js
@@ -29,21 +29,80 @@
     }
   })(typeof(arguments) !== 'undefined' ? arguments : []);
 
+// Experimental debugger API
+// http://code.google.com/chrome/extensions/experimental.debugger.html
+//
+
 // Global script
 
-(function () {
-    if (typeof(window) === 'undefined') return;
-    console.log('VP/X launched');
+if (typeof(window !== 'undefined')) {
+  console.log('VP/X launched');
 
-    var X = chrome.extention;
+  $(function () {
+      var X = chrome.extension;
+      var CM = chrome.contextMenus;
+      var D = chrome.experimental.debugger;
 
-    chrome.contextMenus.create({
-        title: 'Hello to injected code.',
-        onclick: function (info, tab) {
-          console.log('Sending hello to the tab.');
-          chrome.tabs.sendRequest(tab.id, { greeting: 'Hello' },
-            function (v) { console.log(JSON.stringify(v)); });
+      var on_error = function (msg) {
+        return function () {
+          if (X.lastError)
+            console.log(msg + '\nError = ', X.lastError);
+        };
+      };
+
+      var inspected_tabs = [];
+
+      var attach_debugger = function (id) {
+        if (inspected_tabs.indexOf(id) === -1) {
+          console.log('Trying to attach debugger to tab(' + id + ')');
+          D.attach(id,
+            function () {
+              if (X.lastError)
+                console.log('attach_debugger\nError = ', X.lastError);
+              else inspected_tabs.push(id);
+              return !X.lastError;
+            });
+          return true;
         }
-      });
+      };
 
-  })();
+      D.onDetach.addListener(function (id) {
+          inspected_tabs[inspected_tabs.indexOf(id)] = -1;
+        });
+
+      D.onEvent.addListener(function (id, method, params) {
+          console.log('Event@Tab(' + id + ') --- ' + method + '\n', params);
+        });
+
+      var ask_eval = function (id, e) {
+        D.sendRequest(id, 'Runtime.evaluate', { expression: e },
+          function (r) {
+            if (r && r.result)
+              console.log('' + e + ' => ', r.result.description);
+            else if (!r) on_error('ask_eval: ')();
+          });
+      };
+
+      CM.create({
+          title: 'Evaluate fact(6)',
+          onclick: function (info, tab) {
+            var id = tab.id;
+            if (attach_debugger(id)) {
+              ask_eval(id, '1 + 1');
+              ask_eval(id, [
+                'var fact = function (n) {',
+                '    if (n === 0) return 1;',
+                '    else return n * fact(n - 1);',
+                '  }'].join('\n'));
+              ask_eval(id, [
+                '(function () {',
+                '    var r = fact(6);',
+                '    console.log(r);',
+                '    return r;',
+                '  })();'].join('\n'));
+            }
+          }
+        });
+
+    });
+}
