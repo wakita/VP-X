fc28fb81deaa7c49e404d29907b3b35f5f9e41de
Ken Wakita <wakita@is.titech.ac.jp>
2011-06-20 10:34:16 +0900

    デバッガの開始，停止機能の実装．
    TODO: 以下のバグを潰すこと
    - inspected_tabs で，デバッガを装着済みのタブを記録しているが，
    ++ 該当ページで Web Inspector を起動したときのイベントを取ることができない．
    ++ タブが閉じられたときの挙動を検査していない

--- chrome.x/vpx.js
+++ chrome.x/vpx.js
@@ -29,21 +29,87 @@
     }
   })(typeof(arguments) !== 'undefined' ? arguments : []);
 
+// Experimental debugger API
+// http://code.google.com/chrome/extensions/experimental.debugger.html
+//
 // Global script
 
-(function () {
-    if (typeof(window) === 'undefined') return;
-    console.log('VP/X launched');
+if (typeof(window) !== 'undefined') {
+  (function () {
+      console.log('VP/X launched');
 
-    var X = chrome.extention;
+      var X = chrome.extension;
+      var CM = chrome.contextMenus;
+      var D = chrome.experimental.debugger;
 
-    chrome.contextMenus.create({
-        title: 'Hello to injected code.',
-        onclick: function (info, tab) {
-          console.log('Sending hello to the tab.');
-          chrome.tabs.sendRequest(tab.id, { greeting: 'Hello' },
-            function (v) { console.log(JSON.stringify(v)); });
+      var on_error = function (msg) {
+        return function () {
+          if (X.lastError)
+            console.log(msg + '\nError: ', X.lastError.message);
+        };
+      };
+
+      var check_error = function (err_msg) {
+        if (X.lastError) {
+          console.log(err_msg + '\nError = ', X.lastError);
+          throw X.lastError;
         }
-      });
+      }
 
-  })();
+      var inspected_tabs = [];
+
+      var attach_debugger = function (id) {
+        if (inspected_tabs.indexOf(id) === -1) {
+          D.attach(id,
+            function () {
+              check_error('attach_debugger');
+              inspected_tabs.push(id);
+            });
+        } else
+          throw 'Another debugger is already attached to tab(' + id + ')';
+      };
+
+      var detach_debugger = function (id) {
+        if (inspected_tabs.indexOf(id) >= 0) {
+          D.detach(id,
+            function () {
+              check_error('detach_debugger');
+              inspected_tabs[inspected_tabs.indexOf(id)] = -1;
+            });
+        } else throw 'A debugger is not attached';
+      }
+
+      D.onDetach.addListener(function (id) {
+          inspected_tabs[inspected_tabs.indexOf(id)] = -1;
+        });
+
+      D.onEvent.addListener(function (id, method, params) {
+          console.log('Event@Tab(' + id + ')\n', method, '\n', params);
+        });
+
+      CM.create({
+          title: 'Attach debugger',
+          onclick: function (info, tab) {
+            try {
+              attach_debugger(tab.id);
+              console.log('Debugger attached');
+            } catch (e) {
+              console.log('Debugger attachment failed:\n', e);
+            }
+          }
+        });
+
+      CM.create({
+          title: 'Detach debugger',
+          onclick: function (info, tab) {
+            try {
+              detach_debugger(tab.id);
+              console.log('Debugger detached');
+            } catch (e) {
+              console.log('Debugger detachment failed:\n', e);
+            }
+          }
+        });
+
+    })()
+};
